7fbba99709ece0f980d4a3c85d47d2354c024b36
waleedrehman31 <waleedrehman31@gmail.com>
2024-01-11 14:52:56 +0500

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:attandance_management_system/auth/login_or_register.dart';
+import 'package:attandance_management_system/firebase_options.dart';
 import 'package:attandance_management_system/theme/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
