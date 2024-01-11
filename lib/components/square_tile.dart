5d889c0dfa21f7970b2d900ee623906ab6a24ad7
waleedrehman31 <waleedrehman31@gmail.com>
2024-01-11 16:39:29 +0500

    create home page

--- lib/components/square_tile.dart
+++ lib/components/square_tile.dart
@@ -1,24 +1,47 @@
 import 'package:flutter/material.dart';
 
 class SquareTile extends StatelessWidget {
-  final String imagePath;
-  const SquareTile({
-    super.key,
-    required this.imagePath,
-  });
+  // final String imagePath;
+  final String text;
+  final IconData icon;
+  final Function()? onTap;
+  final String date;
+  const SquareTile(
+      {super.key,
+      required this.icon,
+      required this.text,
+      required this.onTap,
+      required this.date});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.white),
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.grey[200],
-      ),
-      child: Image.asset(
-        imagePath,
-        height: 40,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.grey[200],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon,
+              size: 40,
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Text(text),
+            Text(date),
+          ],
+        ),
+        // child: Image.asset(
+        //   imagePath,
+        //   height: 40,
+        // ),
       ),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,75 @@
 import 'package:attandance_management_system/components/my_drawer.dart';
+import 'package:attandance_management_system/components/square_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String currentDate = DateFormat('dd-MM-yyyy').format(DateTime.now());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: const Text("Home"),
       ),
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SquareTile(
+                  onTap: () {},
+                  icon: Icons.assignment_turned_in_sharp,
+                  text: "Mark Attandance",
+                  date: currentDate,
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                SquareTile(
+                  onTap: () {},
+                  icon: Icons.add,
+                  text: "Leave Request",
+                  date: currentDate,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SquareTile(
+                  onTap: () {},
+                  icon: Icons.remove_red_eye_outlined,
+                  text: "View Progress",
+                  date: "",
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                SquareTile(
+                  onTap: () {},
+                  icon: Icons.person_2_outlined,
+                  text: "P R O F I L E",
+                  date: "",
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
